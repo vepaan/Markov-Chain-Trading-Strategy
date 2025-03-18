60bb7b5ef33e2be2ceeda5152f4382b11ed8f790
vepaan <141347566+vepaan@users.noreply.github.com>
2025-03-18 11:21:22 -0500

    built markov chain module

--- markov.cpp
+++ markov.cpp
@@ -8,8 +8,6 @@ using namespace std;
 class MarkovChain {
 private:
     unordered_map<double, unordered_map<double, long long>> chain;
-    unordered_map<double, tuple<double, long long>> next; // stores the most probable next state
-    unordered_map<double, long long> sums; // sums for weighted average
     long long size;
 
 public:
@@ -25,30 +23,33 @@ public:
         if (chain.find(state1) == chain.end()) {
             cout << "Inserted new state pair" << endl;
             size += 1;
-
-            tuple<double, long long> tpl = make_tuple(state2, 1);
-            next[state1] = tpl;           
         }
 
         chain[state1][state2] += 1;
-
-        long long curr = chain[state1][state2];
-        tuple<double, long long> tpl = next[state1];
-
-        if (curr > get<1>(tpl)) {
-            tpl = make_tuple(state2, curr);
-            next[state1] = tpl;
-        }
-
-        sums[state1] += 1;
     }
 
     double probableNextAggresive(double state) {
         if (chain.find(state) == chain.end()) {
             cout << "State doesn't exist in chain" << endl;
             return -1;
+        } else {
+            double res = 0;
+            long long maxCount = 0;
+            long long total = 0;
+
+            for (auto &c: chain[state]) {
+                maxCount = max(maxCount, c.second);
+            }
+
+            for (auto &c: chain[state]) {
+                if (c.second == maxCount) {
+                    res += (c.first);
+                    total += 1;
+                }
+            }
+
+            return res / total;
         }
-        return get<0>(next[state]);
     }
 
     double probableNextSafe(double state) {
@@ -56,11 +57,15 @@ public:
             cout << "State doesn't exist in chain" << endl;
             return -1;
         } else {
-            double res;
+            double res = 0;
+            double sum = 0;
+
             for (auto &c: chain[state]) {
-                res += (c.first) * (c.second) / (sums[state]);
+                res += (c.first) * (c.second);
+                sum += (c.second);
             }
-            return res;
+
+            return res / sum;
         }
     }
 };
